6aed13971a903553e0aa59612fbe71775d1c084a
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-02-15 15:54:55 -0500

    changes VPC handling

--- main.tf
+++ main.tf
@@ -14,30 +14,34 @@ provider "google" {
 }
 
 resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
+  for_each                = var.vpcs
+  name                    = each.key
   auto_create_subnetworks = false
   routing_mode            = "REGIONAL"
   delete_default_routes_on_create = true
 }
 
 resource "google_compute_subnetwork" "webapp_subnet" {
-  name          = "webapp"
-  ip_cidr_range = var.webapp_subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc_network.self_link
+  for_each      = var.vpcs
+  name          = "${each.key}-webapp"
+  ip_cidr_range = each.value.webapp_subnet_cidr
+  region        = each.value.region
+  network       = google_compute_network.vpc_network[each.key].self_link
 }
 
 resource "google_compute_subnetwork" "db_subnet" {
-  name          = "db"
-  ip_cidr_range = var.dbapp_subnet_cidr
-  region        = var.region
-  network       = google_compute_network.vpc_network.self_link
+  for_each      = var.vpcs
+  name          = "${each.key}-db"
+  ip_cidr_range = each.value.db_subnet_cidr
+  region        = each.value.region
+  network       = google_compute_network.vpc_network[each.key].self_link
 }
 
 resource "google_compute_route" "webapp_route" {
-  name       = "webapp-route"
-  dest_range = "0.0.0.0/0"
-  network    = google_compute_network.vpc_network.self_link
+  for_each      = var.vpcs
+  name          = "${each.key}-route"
+  dest_range    = "0.0.0.0/0"
+  network       = google_compute_network.vpc_network[each.key].self_link
   next_hop_gateway = "default-internet-gateway"
   tags = ["webapp"]
 }
