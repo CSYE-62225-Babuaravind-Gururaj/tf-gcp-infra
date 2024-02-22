606e5d026373eac60431abe91af35e78105bc0b7
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-02-22 14:11:54 -0500

    modified terraform and added firewall settings

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "project" {
   description = "The GCP project ID"
   type = string
-  default = "csye-6225-terraform"
+  default = "csye-6225-terraform-packer"
 }
 
 variable "region" {
@@ -13,7 +13,7 @@ variable "region" {
 variable "zone" {
     description = "The GCP zone"
     type = string
-    default = "us-central1-c"
+    default = "us-central1-a"
 }
 
 variable "vpc_names" {
@@ -22,6 +22,17 @@ variable "vpc_names" {
   default     = ["vpc1", "vpc2"]
 }
 
+variable "network_routing_mode" {
+  description = "routing mode for VPC"
+  type = string
+  default = "REGIONAL"
+}
+
+variable "next_hop_gateway" {
+  description = "gateway for webapp"
+  type = string
+  default = "default-internet-gateway"
+}
 
 variable "vpcs" {
   description = "Map of VPC configurations"
@@ -42,4 +53,29 @@ variable "dbapp_subnet_cidr" {
     description = "db_subnet description"
     type = string
     default = "10.0.2.0/24"
+}
+variable "instance_vpc_name" {
+  description = "Name of the VPC for the custom instance"
+  type        = string
+  default     = "vpc1"
+}
+variable "machine_type" {
+  description = "Machine type"
+  type = string
+  default = "e2-medium"
+}
+
+variable "size" {
+  description = "Disk size"
+  default = "100"
+}
+
+variable "type" {
+  description = "Boot disk type"
+  default = "pd-standard"
+}
+
+variable "image" {
+  description = "Disk image"
+  default = "centos-stream-8"
 }
