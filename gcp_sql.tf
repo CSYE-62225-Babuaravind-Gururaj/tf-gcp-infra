2918eb2c184118157ed188e00850d90136a75d0f
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-02-26 23:43:56 -0700

    set up sql

---
+++ gcp_sql.tf
@@ -0,0 +1,60 @@
+resource "google_service_networking_connection" "db_private_vpc_conn" {
+  for_each                  = var.vpcs
+  network                   = google_compute_network.vpc[count.index].id
+  service                   = var.SERVICE_NETWORK_API
+  reserved_peering_ranges   = [google_compute_global_address.db_private_ip[each.key].name]
+}
+
+resource "google_sql_database_instance" "database_instance" {
+  for_each            = var.vpcs
+  name                = "${each.key}-database-instance"
+  database_version    = var.database_version
+  depends_on          = [google_service_networking_connection.db_private_vpc_conn]
+  deletion_protection = false
+
+  settings {
+    tier              = var.db_tier
+    availability_type = var.db_availability_type
+    disk_size         = var.db_disk_size
+    disk_type         = var.db_disk_type
+
+    ip_configuration {
+      ipv4_enabled                                  = false
+      private_network                               = google_compute_network.vpc_network[each.key].id
+      enable_private_path_for_google_cloud_services = true
+    }
+
+    backup_configuration {
+      enabled            = true
+      binary_log_enabled = true
+    }
+  }
+}
+
+resource "google_sql_database" "database" {
+  for_each  = var.vpcs
+  name      = "${each.key}-webapp"
+  instance  = google_sql_database_instance.database_instance[each.key].name
+}
+
+resource "google_compute_global_address" "db_private_ip" {
+  for_each      = var.vpcs
+  name          = "${each.key}-db-private-ip"
+  purpose       = var.db_address_purpose
+  prefix_length = var.db_address_prefix
+  address_type  = var.db_address_type
+  network       = google_compute_network.vpc[each.key].id
+}
+
+resource "random_password" "password" {
+  length           = var.pass_Len
+  special          = false
+  //override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource "google_sql_user" "users" {
+  for_each  = var.vpcs
+  name      = var.webapp_name
+  instance  = google_sql_database_instance.database_instance[each.key].name
+  password  = random_password.password.result
+}
---
+++ vpc.tf
@@ -0,0 +1,32 @@
+resource "google_compute_network" "vpc_network" {
+  for_each                = var.vpcs
+  name                    = each.key
+  auto_create_subnetworks = false
+  routing_mode            = var.network_routing_mode
+  delete_default_routes_on_create = true
+}
+
+resource "google_compute_subnetwork" "webapp_subnet" {
+  for_each      = var.vpcs
+  name          = "${each.key}-webapp"
+  ip_cidr_range = each.value.webapp_subnet_cidr
+  region        = each.value.region
+  network       = google_compute_network.vpc_network[each.key].self_link
+}
+
+resource "google_compute_subnetwork" "db_subnet" {
+  for_each      = var.vpcs
+  name          = "${each.key}-db"
+  ip_cidr_range = each.value.db_subnet_cidr
+  region        = each.value.region
+  network       = google_compute_network.vpc_network[each.key].self_link
+}
+
+resource "google_compute_route" "webapp_route" {
+  for_each      = var.vpcs
+  name          = "${each.key}-route"
+  dest_range    = var.dest_range
+  network       = google_compute_network.vpc_network[each.key].self_link
+  next_hop_gateway = var.next_hop_gateway
+  tags = ["webapp"]
+}
