9b2dbbbe488050a55f0b34b5e3102e176e362cee
Babuaravind Gururaj <gururaj.b@northeastern.edu>
2024-02-29 13:16:37 -0500

    resolved userdata script

---
+++ userdata.sh
@@ -0,0 +1,25 @@
+echo "
+[Unit]
+Description=Go Web Application
+After=network.target
+
+[Service]
+User=csye6225
+Group=csye6225
+Environment=DBHOST=${ip_address}
+Environment=DBPORT=${db_port}
+Environment=DBUSER=${username}
+Environment=DBPASS=${password}
+Environment=DBNAME=${db_name}
+ExecStart=/usr/local/bin/webapp
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/webapp.service
+
+sudo chown csye6225:csye6225 /etc/systemd/system/webapp.service
+sudo chmod 750 /etc/systemd/system/webapp.service
+
+sudo systemctl daemon-reload
+sudo systemctl start webapp.service
+sudo systemctl enable webapp.service
+sudo systemctl status webapp.service
--- vm.tf
+++ vm.tf
@@ -1,7 +1,9 @@
 resource "google_compute_instance" "custom_instance" {
+  for_each     = var.vpcs
   name         = "custom-instance-${var.instance_vpc_name}"
   machine_type = var.machine_type
   zone         = var.zone
+
   network_interface {
     subnetwork = google_compute_subnetwork.webapp_subnet[var.instance_vpc_name].self_link
     access_config {
@@ -10,11 +12,23 @@ resource "google_compute_instance" "custom_instance" {
 
   boot_disk {
     initialize_params {
-      image = var.image  # Reference to your existing custom image
+      image = var.image
       size  = var.size
       type  = var.type
     }
+    auto_delete = true
   }
 
   tags = ["webapp"]
+
+  metadata_startup_script = templatefile("${path.module}/userdata.sh", {
+    for_each         = var.vpcs
+    application_port = var.httpport
+    db_port          = var.db_port
+    ip_address       = google_sql_database_instance.database_instance[each.key].ip_address.0.ip_address
+    username         = google_sql_user.users[each.key].name
+    password         = google_sql_user.users[each.key].password
+    db_name          = google_sql_database.database[each.key].name
+  })
+
 }
